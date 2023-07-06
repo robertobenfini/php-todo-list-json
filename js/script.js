bc45cb8fddcea6f3a3d78addaac1a6e29c0dbede
robertobenfini <roberto.benfini@gmail.com>
2023-07-06 14:41:06 +0200

    recuperato-il-file-json-in-php

--- js/script.js
+++ js/script.js
@@ -3,15 +3,32 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-
+            todoItem: '',
+            todoList: [],
+            apiUrl: 'server.php'
         }
     },
 
     mounted() {
-
+        //chiamata axios
+        axios.get(this.apiUrl).then((response) => {
+            this.todoList = response.data
+        })
     },
 
     methods: {
+        updateList() {
+            const data = {
+                todoItem: this.todoItem
+            }
+
+            axios.post(this.apiUrl, data, {
+                headers: { 'Content-Type': 'multipart/form-data' }
+            }).then((response) => {
+                this.todoItem = '';
+                this.todoList = response.data;
+            })
+        },
 
     },
 }).mount('#app');
--- server.php
+++ server.php
@@ -1,5 +1,10 @@
 <?php
 
-    
+    //recupero il contenuto del file json
+    $string = file_get_contents('data/todo_list.json');
 
+    //lo converto in modo che sia visibile da php come array
+    $array = json_decode($string, true);
+    
+    
 ?>
